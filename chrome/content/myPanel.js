412a7d7a073435721874d95e8522fcea5425a02e
ncdesjardins <nathalie.desjardins@polymtl.ca>
2016-05-06 14:07:15 -0400

    Copie du code de LinkInspector pour le support d'inspection.

--- chrome/content/myPanel.js
+++ chrome/content/myPanel.js
@@ -18,6 +18,8 @@ MyPanel.prototype = Obj.extend(Firebug.Panel,
 {
     name: panelName,
     title: "Ally Enhancer",
+    inspectable: true,
+    inspectHighlightColor: "green",
 
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
     // Initialization
@@ -25,6 +27,7 @@ MyPanel.prototype = Obj.extend(Firebug.Panel,
     initialize: function()
     {
         Firebug.Panel.initialize.apply(this, arguments);
+        Firebug.Inspector.addListener(this);
 
         if (FBTrace.DBG_ALLYENHANCER)
             FBTrace.sysout("allyEnhancer; MyPanel.initialize");
@@ -40,11 +43,13 @@ MyPanel.prototype = Obj.extend(Firebug.Panel,
             FBTrace.sysout("allyEnhancer; MyPanel.destroy");
 
         Firebug.Panel.destroy.apply(this, arguments);
+        Firebug.Inspector.removeListener(this);
     },
 
     show: function(state)
     {
         Firebug.Panel.show.apply(this, arguments);
+
 
         if (FBTrace.DBG_ALLYENHANCER)
             FBTrace.sysout("allyEnhancer; MyPanel.show");
@@ -57,8 +62,97 @@ MyPanel.prototype = Obj.extend(Firebug.Panel,
         this.MyTemplate.render(this.panelNode);
 
         // TODO: Render panel content
-    }
+    },
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+    // Inspector API implementation
+
+    startInspecting: function()
+    {
+        if (FBTrace.DBG_ALLYENHANCER)
+            FBTrace.sysout("allyEnhancer; startInspecting()");
+    },
+
+    inspectNode: function(node)
+    {
+        if (FBTrace.DBG_ALLYENHANCER)
+            FBTrace.sysout("allyEnhancer; inspectNode(node: " + node.tagName + ")");
+
+        // LinkInspectorPlate.linkUrl.replace({object: node}, this.panelNode);
+    },
+
+    stopInspecting: function(node, canceled)
+    {
+        if (FBTrace.DBG_ALLYENHANCER)
+            FBTrace.sysout("allyEnhancer; stopInspecting(node: " + node.tagName +
+                ", canceled: " + canceled + ")");
+
+        if (canceled)
+            return;
+
+        if (node.href.indexOf("http") != 0)
+            return;
+
+        // LinkInspectorPlate.linkPreview.replace({object: node}, this.panelNode);
+    },
+
+    supportsObject: function(object, type)
+    {
+        if (object instanceof Element)
+        {
+            if (object.tagName.toLowerCase() == "a")
+                return 1;
+        }
+
+        return 0;
+    },
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+    // Inspector Listener
+
+    onStartInspecting: function(context)
+    {
+        if (FBTrace.DBG_ALLYENHANCER)
+            FBTrace.sysout("allyEnhancer; Listener.onStartInspecting(context: " +
+                context.getTitle() + ")");
+    },
+
+    onInspectNode: function(context, node)
+    {
+        if (FBTrace.DBG_ALLYENHANCER)
+            FBTrace.sysout("allyEnhancer; Listener.onInspectNode(context: " +
+                context.getTitle() + ", node: " + node.tagName + ")");
+    },
+
+    onStopInspecting: function(context, node, canceled)
+    {
+        if (FBTrace.DBG_ALLYENHANCER)
+            FBTrace.sysout("allyEnhancer; Listener.onStopInspecting(context: " +
+                context.getTitle() + ", node: " + node.tagName + ", canceled: " +
+                canceled + ")");
+    },
 });
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+/* Ne pas oublier de mettre les classes css dans le fichier css.
+var LinkInspectorPlate = domplate(
+{
+    linkUrl:
+        DIV({"class": "linkUrl"},
+            "$object.href"
+        ),
+
+    linkPreview:
+        IFRAME({"class": "linkPreview", "src": "$object.href"}),
+
+    defaultContent:
+        DIV({"class": "defaultContent"},
+            "Use Firebug Inspector and try to inspect a link on the current page."
+        )
+});
+*/
+
 
 // ********************************************************************************************* //
 // Panel UI (Domplate)
